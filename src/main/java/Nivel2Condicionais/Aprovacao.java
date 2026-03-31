0413acc362472b8f40c81714fbdb843fa685b726
Tiago Pitombeira <tiagopitombeira1984@gmail.com>
2026-03-31 15:49:03 -0300

    projeto-exercícios

---
+++ src/main/java/Nivel2Condicionais/Aprovacao.java
@@ -0,0 +1,32 @@
+package Nivel2Condicionais;
+
+import java.util.Scanner;
+
+public class Aprovacao {
+
+	public static void main(String[] args) {
+		
+		try(Scanner sc = new Scanner(System.in)){
+			
+			double nota;
+			
+			System.out.print("Digite a nota: ");
+			if(!sc.hasNextDouble()) {
+				System.out.print("Valor invalido");
+				return;
+			}
+			nota = sc.nextDouble();
+			
+			if(nota >= 7) {
+				System.out.print("Aprovado");
+			}else if(nota >= 5) {
+				System.out.print("Recuperação");
+			}else {
+				System.out.print("Reprovado");
+			}
+			
+		}
+
+	}
+
+}
--- src/main/java/Nivel2Condicionais/Exe5MaiorDeTresNumeros.java
+++ src/main/java/Nivel2Condicionais/Exe5MaiorDeTresNumeros.java
@@ -10,22 +10,36 @@ public class Exe5MaiorDeTresNumeros {
 			
 			int a, b, c, maior;
 			
-			System.out.print("Digite Três Numeros: ");
+			System.out.println("Verifique entre os numeros digitado o maior");
 			
-			if(sc.hasNextInt()) {
-				a = sc.nextInt();
-				b = sc.nextInt();
-				c = sc.nextInt();
-				
-				maior = a;
-				
-				if(b > maior) maior = b;
-				if(c > maior) maior = c;
-				
-				System.out.println("Maior número: "+ maior);
-			}else {
-				System.out.println("Valor invaliado. Entre com um numeto inteiro!");
+			System.out.print("Digite o Primeiro Número: ");
+			if(!sc.hasNextInt()) {
+				System.out.println("Valor invalido para o primeiro numero");
+				return;
 			}
+			a = sc.nextInt();
+			
+			System.out.print("Digite o Segundo Numero: ");
+			if(!sc.hasNextInt()) {
+				System.out.println("Valor invalido para o segundo numero");
+				return;
+			}
+			b = sc.nextInt();
+			
+			System.out.print("Digite o Terceiro Número: ");
+			if(!sc.hasNextInt()) {
+				System.out.println("Valor invalido para o terceiro numero");
+				return;
+			}
+			c = sc.nextInt();
+			
+			maior = a;
+			
+			if(b > maior) maior = b;
+			if(c > maior) maior = c;
+			
+			System.out.println("Maior número: " + maior);
+			
 		}
 
 	}
