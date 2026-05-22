bc0dcaec4e5d2256ef7597b2876bbc9c3add59b2
Tiago Pitombeira <tiagopitombeira1984@gmail.com>
2026-05-22 15:02:09 -0300

    Projeto-Exercícios

--- src/main/java/Nivel3Repeticao/Exe8Contagem.java
+++ src/main/java/Nivel3Repeticao/Exe8Contagem.java
@@ -13,3 +13,33 @@ public class Exe8Contagem {
 	}
 
 }
+
+//-----------------------------------//
+//Outras Forma de Fazer//
+//-----------------------------------//
+
+/*package Nivel4Arrays;
+
+import java.util.Scanner;
+
+public class Exe11Nomes {
+
+	public static void main(String[] args) {
+		
+		int i;
+		int volta = 0;
+		
+		try(Scanner input = new Scanner(System.in)){
+		
+			System.out.print("Digite a Quantidade de Voltas: ");
+			volta = input.nextInt();
+			
+			for(i = 0; i <= volta; i++) {
+				System.out.println(i);
+			}
+			
+		}	
+			
+	}
+		
+}*/
---
+++ src/main/java/Nivel4Arrays/Exe11Nomes.java
@@ -0,0 +1,17 @@
+package Nivel4Arrays;
+
+//import java.util.Scanner;
+
+public class Exe11Nomes {
+
+	public static void main(String[] args) {
+		
+		String[] nomes = {"tiago","waleska","joão","daniel"};
+		
+		for(String nome : nomes) {
+			System.out.println(nome);
+		}
+		
+	}
+		
+}
